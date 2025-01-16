fb5cf5b6c3f61a8489a033b52e366ab838c42dfa
Amitmagdum123 <1910059@ritindia.edu>
2025-01-16 16:01:30 +0530

    Add files via upload

---
+++ Colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+    primaryGradient: "linear-gradient(135deg, #6a11cb 0%, #2575fc 100%)", //"#f4f7fc"
+    PrimaryBackgroundColor: "#f4f7fc",
+    SecondaryBackgroundColor:"#ffffff"
+  };
